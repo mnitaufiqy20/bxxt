9842a6830ac8f8f51ea8dcc3a1838d4f5db8a5cd
xuguanjie <18217678895@139.com>
2013-03-20 11:13:57 +0800

    修改流程文件上传

--- grails-app/controllers/com/chd/bx/flowManage/FlowSheetInfuseController.groovy
+++ grails-app/controllers/com/chd/bx/flowManage/FlowSheetInfuseController.groovy
@@ -19,17 +19,15 @@ class FlowSheetInfuseController {
         render(view: '/flowManage/flowSheetInfuse', model: [strMsg: strMsg])
     }
     def flowSheetInfusePath(params){
-//        def filePath = params["filePath"]
         def strMsg = ""
         def f = request.getFile("filePath");
-        if((!f.empty)&&(f.size<200000)){//判断文件不为空且小于设定的大小
+        if(!f.empty){//判断文件不为空
             String name = f.getOriginalFilename()//获得文件原始的名称
             def file = new File("grails-app/Upload/"+name)
             if(!file.exists()){
                 file.mkdirs()//目录不存在则创建
             }
-            f.transferTo( file )
-            System.out.print(file.getAbsolutePath())
+            f.transferTo(file)
             try{
                 workflowFactory.applyWorkflow(processEngine,file.getAbsolutePath())
                 strMsg = "发布成功！"
@@ -37,24 +35,9 @@ class FlowSheetInfuseController {
                 e.printStackTrace()
                 strMsg = "发布失败！"
             }
-            //将得到的文件转化成指定的文件                                                                          flash.message=”文件正好”
-            //def myFileInstance = new MyFile(filePath:file.getAbsolutePath())
-           // myFileInstance.save(flush:true)//保存文件的地址
-           // redirect(action:”list”)
-        }/*else if(f.empty){
-            flash.message =”文件不能为空”
-            render(view:”upload”)
-        }else if(f.size>=200000){
-            flash.message =”文件有点大”
-            render(view:”upload”)
-        }*/
-
-
-
-
-
-
-
+        }else if(f.empty){
+            flash.message ="文件不能为空"
+        }
         render(view: '/flowManage/flowSheetInfuse', model: [strMsg: strMsg])
     }
 }
